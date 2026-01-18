c50c356c8ff0d1a3878247725fbbecd4fdc52cf3
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-01-17 19:40:30 -0500

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// 33:21
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to my movies app</Text>
+    </View>
+  );
+}
